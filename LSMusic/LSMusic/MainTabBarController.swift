bcbaa8aeb2aa477b18c84f584614dea75900964c
Stanislav <lemeshaevs@gmail.com>
2020-07-15 11:25:00 +0300

    Добавил файлы и иконки

---
+++ LSMusic/LSMusic/MainTabBarController.swift
@@ -0,0 +1,9 @@
+//
+//  MainTabBarController.swift
+//  LSMusic
+//
+//  Created by Станислав Лемешаев on 15.07.2020.
+//  Copyright © 2020 Станислав Лемешаев. All rights reserved.
+//
+
+import Foundation
