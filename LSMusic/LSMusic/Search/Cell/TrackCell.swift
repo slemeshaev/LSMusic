5c6f270ed873845453f92469bf5c6318a02db709
Stanislav <lemeshaevs@gmail.com>
2020-07-17 17:05:23 +0300

    Создание ячейки для трека с помощью xib

--- LSMusic/LSMusic/Search/Cell/TrackCell.swift
+++ LSMusic/LSMusic/Search/Cell/TrackCell.swift
@@ -6,4 +6,32 @@
 //  Copyright © 2020 Станислав Лемешаев. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+protocol TrackCellViewModel {
+    var iconUrlString: String? { get }
+    var trackName: String { get }
+    var artistName: String { get }
+    var collectionName: String { get }
+}
+
+class TrackCell: UITableViewCell {
+    
+    static let reuseId = "TrackCell"
+    
+    @IBOutlet weak var trackImageView: UIImageView!
+    @IBOutlet weak var trackNameLabel: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var collectionNameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    func set(viewModel: TrackCellViewModel) {
+        trackNameLabel.text = viewModel.trackName
+        artistNameLabel.text = viewModel.artistName
+        collectionNameLabel.text = viewModel.collectionName
+    }
+    
+}
