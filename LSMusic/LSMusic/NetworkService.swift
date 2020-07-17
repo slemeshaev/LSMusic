85d82071fbfe7e43747c0377c047582283e83652
Stanislav <lemeshaevs@gmail.com>
2020-07-17 11:45:20 +0300

    Создал файл для сетевой логики

---
+++ LSMusic/LSMusic/NetworkService.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkService.swift
+//  LSMusic
+//
+//  Created by Станислав Лемешаев on 17.07.2020.
+//  Copyright © 2020 Станислав Лемешаев. All rights reserved.
+//
+
+import Foundation
