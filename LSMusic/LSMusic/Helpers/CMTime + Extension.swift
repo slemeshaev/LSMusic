e64dd199d7a0cfa7033c102e0fd42cd3be1d9f6f
Stanislav <lemeshaevs@gmail.com>
2020-07-29 15:36:34 +0300

    Добавил extension для CMTime

---
+++ LSMusic/LSMusic/Helpers/CMTime + Extension.swift
@@ -0,0 +1,9 @@
+//
+//  CMTime + Extension.swift
+//  LSMusic
+//
+//  Created by Станислав Лемешаев on 29.07.2020.
+//  Copyright © 2020 Станислав Лемешаев. All rights reserved.
+//
+
+import Foundation
