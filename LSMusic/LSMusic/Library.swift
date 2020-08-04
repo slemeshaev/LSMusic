faa45d007bf13a652bb772957ce8f676484f3b4e
Stanislav <lemeshaevs@gmail.com>
2020-08-04 15:58:41 +0300

    Файл для любимых треков

---
+++ LSMusic/LSMusic/Library.swift
@@ -0,0 +1,21 @@
+//
+//  Library.swift
+//  LSMusic
+//
+//  Created by Станислав Лемешаев on 04.08.2020.
+//  Copyright © 2020 Станислав Лемешаев. All rights reserved.
+//
+
+import SwiftUI
+
+struct Library: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Library_Previews: PreviewProvider {
+    static var previews: some View {
+        Library()
+    }
+}
