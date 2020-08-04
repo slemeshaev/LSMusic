b2c387238c2fb494350115cbb8de023add20c8d5
Stanislav <lemeshaevs@gmail.com>
2020-08-04 20:06:03 +0300

    Сделал extension для userdefaults

---
+++ LSMusic/LSMusic/Search/Cell/UserDefaults+Extension.swift
@@ -0,0 +1,9 @@
+//
+//  UserDefaults+Extension.swift
+//  LSMusic
+//
+//  Created by Станислав Лемешаев on 04.08.2020.
+//  Copyright © 2020 Станислав Лемешаев. All rights reserved.
+//
+
+import Foundation
